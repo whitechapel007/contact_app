e8eb60ba4dc2e3007f82efce4acabba07888515f
whitechapel007 <ebukareed@gmail.com>
2022-01-06 16:07:51 +0100

    deploy soon

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 const app = express();
 
 //init middleware
@@ -11,15 +13,18 @@ app.use(express.json({ extended: false }));
 
 connectDB();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API.. " })
-);
-
 //define routes
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}   `));
